8f6be6e87ccbe4b28be99566718b22b67e42d988
Prince527 <prince527@duck.com>
2026-01-14 13:29:04 -0400

    Add 5 packages

---
+++ pkgs/pcsx2/default.nix
@@ -0,0 +1,18 @@
+# NOTE: MacOS version is untested
+{
+  stdenvNoCC,
+  pcsx2-bin,
+  fetchurl,
+  pcsx2,
+  lib,
+  ...
+}:
+if stdenvNoCC.isDarwin
+then let
+  ver = lib.helper.read ./version.json;
+in (pcsx2-bin.override {
+  src = fetchurl (lib.helper.getSingle ver);
+
+  meta.platforms = lib.platforms.darwin;
+})
+else pcsx2
