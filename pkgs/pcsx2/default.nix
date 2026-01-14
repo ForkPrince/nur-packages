ef12490915d51c1e8c513957e2b88cbc4ef8b1bc
Prince527 <prince527@duck.com>
2026-01-14 13:35:06 -0400

    Patch

--- pkgs/pcsx2/default.nix
+++ pkgs/pcsx2/default.nix
@@ -12,6 +12,15 @@ then let
   ver = lib.helper.read ./version.json;
 in pcsx2-bin.overrideAttrs (old: {
   src = fetchurl (lib.helper.getSingle ver);
+  inherit (ver) version;
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/Applications
+    app=$(find . -maxdepth 2 -name "*.app" -type d | head -n1)
+    cp -R "$app" $out/Applications/
+    runHook postInstall
+  '';
 
   meta.platforms = lib.platforms.darwin;
 })
