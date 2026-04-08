b600ae6f8107ae9244bc0157c72e23565c517069
Prince527 <prince527@duck.com>
2026-04-08 10:42:05 -0300

    Custom Maintainers

---
+++ lib/maintainers.nix
@@ -0,0 +1,42 @@
+{
+  Prinky = {
+    name = "Prinky";
+    github = "Prince527Github";
+    githubId = 75485262;
+  };
+  Ev357 = {
+    name = "Lucas Gager";
+    github = "Ev357";
+    githubId = 113623019;
+  };
+  JoyfulCat = {
+    name = "JoyfulCat";
+    github = "JoyfulCat";
+    githubId = 121145249;
+  };
+  WoutFontaine = {
+    name = "WoutFontaine";
+    github = "WoutFontaine";
+    githubId = 231103959;
+  };
+  crertel = {
+    name = "crertel";
+    github = "crertel";
+    githubId = 1707779;
+  };
+  jshcmpbll = {
+    name = "Joshua Campbell";
+    github = "jshcmpbll";
+    githubId = 16374374;
+  };
+  edmundmiller = {
+    name = "Edmund Miller";
+    github = "edmundmiller";
+    githubId = 20095261;
+  };
+  zh4ngx = {
+    name = "Andy Zhang";
+    github = "zh4ngx";
+    githubId = 1329212;
+  };
+}
