d6b67cb87d76182c01d9771bb2af0dd307c361ac
Prince527 <prince527@duck.com>
2026-04-08 11:01:04 -0300

    Cleanup

--- lib/maintainers.nix
+++ lib/maintainers.nix
@@ -19,24 +19,4 @@
     github = "WoutFontaine";
     githubId = 231103959;
   };
-  crertel = {
-    name = "crertel";
-    github = "crertel";
-    githubId = 1707779;
-  };
-  jshcmpbll = {
-    name = "Joshua Campbell";
-    github = "jshcmpbll";
-    githubId = 16374374;
-  };
-  edmundmiller = {
-    name = "Edmund Miller";
-    github = "edmundmiller";
-    githubId = 20095261;
-  };
-  zh4ngx = {
-    name = "Andy Zhang";
-    github = "zh4ngx";
-    githubId = 1329212;
-  };
 }
