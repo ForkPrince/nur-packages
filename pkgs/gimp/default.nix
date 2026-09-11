daf40decf7900402cdc0a26f15f201de381af14c
Prince527 <prince527@duck.com>
2026-09-11 13:09:26 -0300

    Patch GIMP?
    Hopefully

--- pkgs/gimp/default.nix
+++ pkgs/gimp/default.nix
@@ -2,7 +2,7 @@
   gimp-with-plugins,
   stdenvNoCC,
   fetchurl,
-  undmg,
+  _7zz,
   lib,
 }:
 if stdenvNoCC.hostPlatform.isDarwin
@@ -15,7 +15,13 @@ in
 
     src = fetchurl (lib.helper.getPlatform stdenvNoCC.hostPlatform.system ver);
 
-    nativeBuildInputs = [undmg];
+    nativeBuildInputs = [_7zz];
+
+    unpackPhase = ''
+      runHook preUnpack
+      7zz x "$src" -snld
+      runHook postUnpack
+    '';
 
     meta = {
       description = "GNU Image Manipulation Program";
